8e7f20f21ae1654411a5ef72d5a2afd7d57f7bdb
Aakash-Krishnan <mrskys156@gmail.com>
2024-05-18 12:04:03 +0530

    implemented search feature

--- weather.js
+++ weather.js
@@ -2,6 +2,11 @@ const celsius = document.getElementById("celsius");
 const region = document.getElementById("region");
 const humidity = document.getElementById("humidity");
 const windSpeed = document.getElementById("wind-speed");
+const cloudImg = document.getElementById("cloud-img");
+const container = document.getElementById("container");
+
+const searchBtn = document.getElementById("search-btn");
+const searchInput = document.getElementById("search-input");
 
 const getCurrentGeoLocation = () => {
   return new Promise((resolve, reject) => {
@@ -18,7 +23,24 @@ const getCurrentGeoLocation = () => {
   });
 };
 
-const getLocationDetails = async ({ lat, long }) => {
+searchBtn.addEventListener("click", async (e) => {
+  const value = searchInput.value;
+  if (value.trim()) {
+    const res = await searchWeatherData(value);
+    console.log("SEACRH", res);
+
+    const humidity = res.current.humidity;
+    const name = res.location.name;
+    const temp = res.current.temp_c;
+    const windSpeed = res.current.wind_kph;
+    const localTime = res.location.localtime;
+    updateDomElements({ humidity, name, temp, windSpeed, localTime });
+  } else {
+    alert("Enter a location to seach...");
+  }
+});
+
+const getWeatherLocationDetails = async ({ lat, long }) => {
   const url = `http://api.weatherapi.com/v1/current.json?key=95f4d3f7b64944a3a6b143731240305&q=${lat},${long}&aqi=no
     `;
   try {
@@ -30,17 +52,45 @@ const getLocationDetails = async ({ lat, long }) => {
   }
 };
 
+const searchWeatherData = async (search) => {
+  console.log(search);
+  const url = `http://api.weatherapi.com/v1/current.json?key=95f4d3f7b64944a3a6b143731240305&q=${search}&aqi=no`;
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    return error;
+  }
+};
+
 const updateDomElements = (data) => {
-  console.log(data);
   celsius.innerHTML = `${data.temp}&degC`;
   region.innerText = data.name;
   humidity.innerHTML = `${data.humidity}%`;
   windSpeed.innerHTML = `${data.windSpeed} Km/h`;
+
+  const hour = new Date(data.localTime).getHours();
+  console.log("HOUR", hour);
+
+  if (hour < 4 || hour >= 18) {
+    console.log("Night", cloudImg);
+    container.style.background = "rgb(35,36,0)";
+    container.style.background =
+      "linear-gradient(153deg, rgba(35,36,0,1) 0%, rgba(0,2,14,1) 0%, rgba(0,20,46,1) 46%, rgba(0,212,255,1) 99%)";
+    cloudImg.src = "./assests/moon.png";
+  } else if (hour >= 4 || hour < 18) {
+    console.log("DAY", cloudImg);
+    container.style.background = "rgb(35,36,0)";
+    container.style.background =
+      "linear-gradient(140deg, rgba(35,36,0,1) 0%, rgba(248,255,207,20) 0%, rgba(0,212,255,1) 99%)";
+    cloudImg.src = "./assests/sun.png";
+  }
 };
 
 window.addEventListener("load", (e) => {
   getCurrentGeoLocation()
-    .then(getLocationDetails)
+    .then(getWeatherLocationDetails)
     .then((res) => {
       const humidity = res.current.humidity;
       const name = res.location.name;
